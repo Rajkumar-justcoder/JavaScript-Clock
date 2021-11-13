367ee46fd16b340b5d7259f4e9bc342aa187f9ee
Rajkumar-stackcoder <meet.r.graphicat10@gmail.com>
2021-11-13 17:26:16 +0530

    inital

--- src/clock.js
+++ src/clock.js
@@ -1,15 +1,19 @@
-setInterval(() => {
-    const clock = new Date;
-    let hour;
-    let minutes;
-    let second;
-    let date;
+let hour;
+let minutes;
+let second;
+let date;
+
+function clockfun() {
+    const clock = new Date();
     hour = clock.getHours();
     minutes = clock.getMinutes();
     second = clock.getSeconds();
     date = clock.toLocaleDateString();
+    // let usaTime = clock.toLocaleString("en-US", {timeZone: "UTC"});
+    // console.log(usaTime);
     var session = "AM";
     if (hour == 0) {
+        s
         hour = 12;
     }
     if (hour > 12) {
@@ -20,9 +24,9 @@ setInterval(() => {
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     second = (second < 10) ? "0" + second : second;
     var Clock = hour + ' : ' + minutes + ' : ';
-
     document.querySelector('.time').innerHTML = Clock;
     document.querySelector('.sec').innerHTML = second;
-    document.querySelector('.date').innerHTML = date;
-}, 1000);
+    document.querySelector('.date').innerHTML = date
+}
+setInterval(clockfun, 1000);
 
